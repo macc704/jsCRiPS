0681da9e7239db5e741a9328c2305c46812eab79
Eniwder <akabudou0120@yahoo.co.jp>
2015-07-26 05:04:41 +0900

    演習用の外部jsを読み込めるようにした、デザインを少し変更

---
+++ test/examples/2.5.1.1_Curve.js
@@ -0,0 +1,47 @@
+var t = createTurtle();
+
+var length;
+var angle;
+
+length = 5;
+angle = 5;
+
+t.rt(length);
+t.fd(angle);
+t.rt(length);
+t.fd(angle);
+t.rt(length);
+t.fd(angle);
+t.rt(length);
+t.fd(angle);
+t.rt(length);
+t.fd(angle);
+t.rt(length);
+t.fd(angle);
+t.rt(length);
+t.fd(angle);
+t.rt(length);
+t.fd(angle);
+t.rt(length);
+t.fd(angle);
+t.rt(length);
+t.fd(angle);
+
+t.lt(length);
+t.fd(angle);
+t.lt(length);
+t.fd(angle);
+t.lt(length);
+t.fd(angle);
+t.lt(length);
+t.fd(angle);
+t.lt(length);
+t.fd(angle);
+t.lt(length);
+t.fd(angle);
+t.lt(length);
+t.fd(angle);
+t.lt(length);
+t.fd(angle);
+t.lt(length);
+t.fd(angle);
