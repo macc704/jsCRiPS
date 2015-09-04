c613e92817deab69cafb75f4e92fd31576b0d516
Takeuchi Toshiki <cs11054@s.inf.shizuoka.ac.jp>
2015-09-04 17:04:35 +0900

    inputTurtleを作成

---
+++ test/examples/17.2.1.1.1_InputTurtleSample1.js
@@ -0,0 +1,17 @@
+var input = new createInputTurtle();
+input.warp(30, 30);
+// 表示用テキスト
+var text = createTextTurtle("ここに文字が表示されます");
+text.fontsize(14);
+ 
+while (true) {
+	sleep(0.025);// 待つ
+
+	// エンターキーが押されたら
+	if (key() == 13) {
+		text.text(input.text());// 表示用テキストの内容を入力ボックスの内容に変える
+		input.clearText();// 表示用テキストの内容をリセットする
+	}
+ 
+update();// 再描画する
+}
